35ca45f0a1e549db6a6655a848b4fcdcc5e66944
Marcel Martins - SFS <marcel.marques_martins@siemens.com>
2024-09-23 15:15:17 +0100

    update

--- docker-run_sonar-cli.sh
+++ docker-run_sonar-cli.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-docker container run --rm --network=host -e SONAR_HOST_URL="http://localhost:9000" -v "./src:/usr/src" sonarsource/sonar-scanner-cli -Dsonar.projectKey=conversao \
+docker container run --rm --network=host -e SONAR_HOST_URL="http://localhost:9000" -v "./web/app:/app" sonarsource/sonar-scanner-cli -Dsonar.projectKey=php-test \
 -Dsonar.sources=. \
 -Dsonar.host.url=http://localhost:9000 \
--Dsonar.login=sqp_e1bfb37333ccceb52bed70b24ca0a1919ee1f766
+-Dsonar.login=sqp_4a53ac56aa9e5c32ab67e92fedac8dffe856636b
